f559914460830f44a7a9fa8195a0d32e0d229e2b
David Medina <davidamedina619@gmail.com>
2020-10-19 02:43:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -20,7 +20,7 @@ def game_hash
              :blocks => 1
              :slam_dunks => 1
            },
-            {:player_name => "Alan Anderson"
+            {:player_name => "Reggie Evans"
              :shoe => 16
              :points => 22
              :rebounds => 12
