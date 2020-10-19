d85e4a3a6536808064579b512f202b3240f22b47
David Medina <davidamedina619@gmail.com>
2020-10-19 02:28:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,10 +3,26 @@
 def game_hash
   final_hash = {
     :home => {
-      
+      :team_name =>{
+        
+      },
+      :colors => {
+        
+      },
+      :players => {
+        
+      }
     },
     :away => {
-      
+      :team_name =>{
+        
+      },
+      :colors => {
+        
+      },
+      :players => {
+        
+      }
     }
   }
   
