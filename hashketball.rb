1fc7db1c3e67085c0734a0776594130e804ab7c4
David Medina <davidamedina619@gmail.com>
2020-10-19 02:53:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -12,6 +12,7 @@ def game_hash
       :players => {
         [  
           {  :player_name => "Alan Anderson"
+             :number => 0
              :shoe => 16
              :points => 22
              :rebounds => 12
@@ -21,41 +22,45 @@ def game_hash
              :slam_dunks => 1
            },
             {:player_name => "Reggie Evans"
-             :shoe => 16
-             :points => 22
+             :number => 30
+             :shoe => 14
+             :points => 12
              :rebounds => 12
              :assists => 12
-             :steals => 3
-             :blocks => 1
-             :slam_dunks =>
+             :steals => 12
+             :blocks => 12
+             :slam_dunks => 7
             },
             {:player_name => "Brook Lopez"
-             :shoe => 16
-             :points => 22
-             :rebounds => 12
-             :assists => 12
+             :number => 11
+             :shoe => 17
+             :points => 17
+             :rebounds => 19
+             :assists => 10
              :steals => 3
              :blocks => 1
-             :slam_dunks =>
+             :slam_dunks => 15
               
             },
             {:player_name => "Mason Plumlee"
-             :shoe => 16
-             :points => 22
-             :rebounds => 12
-             :assists => 12
+             :number => 1
+             :shoe => 19
+             :points => 26
+             :rebounds => 11
+             :assists => 6
              :steals => 3
-             :blocks => 1
-             :slam_dunks =>
+             :blocks => 8
+             :slam_dunks => 5
             },
-            {:player_name => "Alan Anderson"
-             :shoe => 16
-             :points => 22
-             :rebounds => 12
-             :assists => 12
-             :steals => 3
-             :blocks => 1
-             :slam_dunks =>
+            {:player_name => "Jason Terry"
+             :number => 31
+             :shoe => 15
+             :points => 19
+             :rebounds => 2
+             :assists => 2
+             :steals => 4
+             :blocks => 11
+             :slam_dunks => 1
               
             }]
       }
@@ -68,7 +73,62 @@ def game_hash
         ["Turquoise", "Purple"]
       },
       :players => {
-        
+        [ {
+          :player_name => "Jason Terry"
+             :number => 31
+             :shoe => 15
+             :points => 19
+             :rebounds => 2
+             :assists => 2
+             :steals => 4
+             :blocks => 11
+             :slam_dunks => 1
+        },
+        {
+          :player_name => "Jason Terry"
+             :number => 31
+             :shoe => 15
+             :points => 19
+             :rebounds => 2
+             :assists => 2
+             :steals => 4
+             :blocks => 11
+             :slam_dunks => 1
+        },
+        {
+          :player_name => "Jason Terry"
+             :number => 31
+             :shoe => 15
+             :points => 19
+             :rebounds => 2
+             :assists => 2
+             :steals => 4
+             :blocks => 11
+             :slam_dunks => 1
+        },
+        {
+          :player_name => "Jason Terry"
+             :number => 31
+             :shoe => 15
+             :points => 19
+             :rebounds => 2
+             :assists => 2
+             :steals => 4
+             :blocks => 11
+             :slam_dunks => 1
+        },
+        {
+          :player_name => "Jason Terry"
+             :number => 31
+             :shoe => 15
+             :points => 19
+             :rebounds => 2
+             :assists => 2
+             :steals => 4
+             :blocks => 11
+             :slam_dunks => 1
+        }
+          ]
       }
     }
   }
